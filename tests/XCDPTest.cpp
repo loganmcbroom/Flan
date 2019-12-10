ff6349fd5893f430ba977af66d5954a6d99f855c
loganmcbroom <loganmcbroom@gmail.com>
2019-12-09 21:28:04 -0500

    Updated test file from boys house

--- tests/XCDPTest.cpp
+++ tests/XCDPTest.cpp
@@ -14,7 +14,6 @@ void graph( const std::string & f );
 
 //TODO
 //AA filter synthesis
-//Switch image formats (for jc work)
 //desample ending bug
 //sort PVOC data by frequency, see if output improves
 //Make sure surfaces are supported everywhere they could be
@@ -23,20 +22,24 @@ void graph( const std::string & f );
 void main()
 	{
 	//Audio sine = makeSine( [](double t){ return 440.0*(1+t);} );
-	//PVOC meow = Audio( "Audio/meow.wav" ).convertToPVOC();
-	Audio bah = Synthesis::saw( 2.0, []( double t ){ return 220.0+220.0*t; } )
-	.convertToPVOC()
+	Audio other = Audio( "Audio/bah.wav" );
+	Audio bah = Audio( "Audio/co.wav" )
+	//.setVolume( 0.5 )
+	//Audio bah = Synthesis::saw( 2.0, []( double t ){ return 220.0+220.0*t; } )
+	//.convertToPVOC( 2048, 32 )
 	//.modifyFrequency( []( double t, double f ){ return f*(0.7 + 0.5*sin(16.0*t)); } )
 	//.modifyTime( []( double t, double f ){ return t*1.0 + f/12000.0; } )
-	.getSpectrograph( "temp.tga" )
-	.convertToAudio()
-	.setVolume( 0.8 );
+	//.resonate( 8, .3 )
+	//.getSpectrograph( "temp.bmp" )
+	//.convertToAudio()
+	.convolve( other )
+	.setVolume( 0.7 );
 
 	//RealFunc tvp1 = RealFunc::interpolatePoints( { {0, 2}, {1, 2}, {2, 1}, {3, 2} } );
 	//RealFunc tvp2 = RealFunc::interpolatePoints( { {0, 2}, {1, 2}, {2, 1}, {3, 2} }, Interpolators::sine );
-	//tvp1.graph();
+	//tvp1.graph( "tempBMP.bmp" );
 	
-	graph( "temp.tga" );
+	graph( "temp.bmp" );
 	play( bah );
 	}
 
@@ -52,6 +55,6 @@ void play( const Audio & toPlay )
 
 void graph( const std::string & f )
 	{
-	const std::string command = std::string("start \"C:\\Program Files (x86)\\FastStone Image Viewer\\FSViewer.exe\" ");
+	const std::string command = std::string("start \"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\paint.exe\" ");
 	system( (command + f).c_str() );
 	}
